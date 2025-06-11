1a00ebc2c36491a0b6866104a3aaf9a9b4adfa31
Jonathan Nielsen <jonathan.sparvath@gmail.com>
2025-06-11 16:50:20 +0200

    checking husky again

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
+import { Button } from '../../../packages/ui/src/button';
 
 const a: any = 0;
 
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <Button>My button</Button>
       </body>
     </html>
   );
